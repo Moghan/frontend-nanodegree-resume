4a980eb4a39d83f94cb090bc8330edc09b3c6c49
Moghan <anders.kensby@gmail.com>
2015-12-15 20:17:39 +0100

    Add first version of code for the resume page. Add gulp, browser-sync, sass etc. Configuring guspfile.js

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('js/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['lint', 'scripts']);
+	gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+/*gulp.task('sass', function() {
+	return gulp.src('scss/*.scss')
+		.pipe(sass))
+		.pipe(gulp.dest('css'));
+});*/
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+
+
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'serve']);
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,250 @@
+ bio = {
+	"name" : "Anders Kensby",
+	"role" : "Webdeveloper",
+	"contacts" : {
+		"mobile" : "+46 (0)70 260 96 94", 
+		"email" : "anders.kensby@gmail.com",
+		"github" : "moghan",
+		"twitter" : "no twitter",
+		"location" : "Arvika"
+	},
+	"welcomeMessage" : "Feel free to have a look at my resume.",
+	"skills" : [
+	"Teamleader experience", "Outside the box"
+	],
+	"bioPic" : "http://placebear.com/220/220"
+}
+
+education = {
+	"schools" : [
+	{
+		"name" : "Karlstads universitet",
+		"location" : "Karlstad",
+		"degree" : "BA",
+		"majors" : "CS",
+		"dates" : "2015",
+		"url" : "http://www.kau.se"
+	},
+	{
+		"name" : "Solberga",
+		"location" : "Arvika",
+		"degree" : "BA",
+		"majors" : "CS",
+		"dates" : "2015",
+		"url" : "http://www.kau.se"
+	}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Front-End Nanodegree",
+			"school" : "Udacity",
+			"date" : "2015",
+			"url" : "http://www.udacity.com"
+		}
+	]
+}
+
+work = {
+	"jobs" : [
+		{
+			"employer" : "Wermland Mechanics",
+			"title" : "Montör",
+			"location" : "Stockholm",
+			"dates" : "June 2015 -",			
+			"description" : "Brainless"
+		},
+		{
+			"employer" : "REC Scanmodule",
+			"title" : "Team Leader",
+			"location" : "Göteborg",
+			"dates" : "2008 - 2011",			
+			"description" : "Responsible for..."
+		},
+		{
+			"employer" : "ttttTEST",
+			"title" : "Team Leader",
+			"location" : "Malmö",
+			"dates" : "2008 - 2011",			
+			"description" : "Brainless"
+		}
+
+	]
+}
+
+project = {
+	"projects" : [
+		{
+			"title" : "ObT6",
+			"dates" : "2014",
+			"description" : "My first github project",
+			"image" : "http://www.placebear.com/200/200"
+		},
+		{
+			"title" : "GeneralNeo",
+			"dates" : "2016-",
+			"description" : "EON battlefield emulator.",
+			"image" : "http://www.placebear.com/200/200"
+		},
+		{
+			"title" : "GeneralNeo",
+			"dates" : "2016-",
+			"description" : "EON battlefield emulator.",
+			"image" : "http://www.placebear.com/200/200"
+		},
+		{
+			"title" : "ObT6",
+			"dates" : "2014",
+			"description" : "My first github project",
+			"image" : "http://www.placebear.com/200/200"
+		},
+		{
+			"title" : "3rd",
+			"dates" : "1984",
+			"description" : "Great movie.",
+			"image" : "http://www.placebear.com/200/200"
+		}
+	]
+}
+
+
+
+
+
+bio.display = function () {
+	//Skills
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+
+		for (var i = 0 ; i < bio.skills.length ; i++) {
+			var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+			$("#skills").append(formattedSkill);
+		}
+
+	}
+
+	//Contacts
+	var keyNames = Object.keys(bio.contacts);
+	
+	for (var i = 0 ; i < keyNames.length ; i++) {
+		var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contacts[keyNames[i]]);
+		formattedContact = formattedContact.replace("%contact%", keyNames[i]);
+
+		$("#topContacts").append(formattedContact);
+	}
+
+	//Role
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
+
+	//Name
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").prepend(formattedName);
+
+	//Welcome message
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
+	$("#header").append(formattedWelcomeMsg);
+	console.log(formattedWelcomeMsg);
+
+
+}
+
+bio.display();
+
+
+
+
+education.display = function () {
+	for (school in education.schools)
+	{
+		$("#education").append(HTMLschoolStart);
+
+		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+
+
+		var formattedEntry = formattedName + formattedDegree + formattedDates + formattedMajor;
+console.log(formattedEntry);
+		$(".education-entry:last").append(formattedEntry);
+	}
+}
+
+education.display();
+
+
+project.display = function () {
+	for (prj in project.projects)
+	{
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", project.projects[prj].title);
+		var formattedDates = HTMLprojectDates.replace("%data%", project.projects[prj].dates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", project.projects[prj].description);
+		var formattedImage = HTMLprojectImage.replace("%data%", project.projects[prj].image);
+
+
+		var formattedEntry = formattedTitle + formattedDates + formattedDescription + formattedImage;
+console.log(formattedEntry);
+		$(".project-entry:last").append(formattedEntry);
+	}
+}
+
+project.display();
+
+
+
+
+
+
+
+work.display = function () {
+	console.log("work.display");
+	for (job in work.jobs)
+	{
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+
+		var formattedWorkEntry = formattedEmployer + formattedTitle + formattedDates + formattedLocation + formattedDescription;
+
+		$(".work-entry:last").append(formattedWorkEntry);
+	}
+}
+
+work.display();
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+
+	logClicks(x,y);
+});
+ 
+function locationizer(work_obj) {
+	var jobLocationArray = [];
+
+	for (job in work.jobs) {
+		jobLocationArray.push(work.jobs[job].location);
+	}
+
+	return jobLocationArray;
+}
+
+
+
+
+bio.display();
+
+
+$("#mapDiv").append(googleMap);
+
+var locArr = locationizer(work);
+*/
